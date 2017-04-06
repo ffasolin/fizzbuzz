b0aa702cb140e66b7400b3a1b2566a327fae2c6d
Filipe Fasolin <ffasolin@gmail.com>
2017-04-06 17:21:10 +0100

    passes second test

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,4 +4,8 @@ describe "fizzbuzz" do
   it 'should return "fizz" if passed 3' do
     expect(fizzbuzz(3)).to eq "fizz"
   end
+
+  it 'should return "buzz" if passed 5' do
+    expect(fizzbuzz(5)).to eq "buzz"
+  end
 end
