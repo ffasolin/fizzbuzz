40fde83ddedbec5eefc0ed52614f8acb15ac5504
Filipe Fasolin <ffasolin@gmail.com>
2017-04-06 17:26:35 +0100

    Passes tests for divisors of 3 and 5

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -8,4 +8,8 @@ describe "fizzbuzz" do
   it 'should return "buzz" if passed 5' do
     expect(fizzbuzz(5)).to eq "buzz"
   end
+
+  it 'should return "fizzbuzz" if passed 15' do
+    expect(fizzbuzz(15)).to eq "fizzbuzz"
+  end
 end
