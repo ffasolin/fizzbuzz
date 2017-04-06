68775eb635f645f0e960e6938601fcc4e18e1d22
Filipe Fasolin <ffasolin@gmail.com>
2017-04-06 17:30:06 +0100

    returns number if not divisible by 3 and or 5

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -2,4 +2,5 @@ def fizzbuzz(n)
   return "fizzbuzz" if n % 15 == 0
   return "fizz" if n % 3 == 0
   return "buzz" if n % 5 == 0
+  n
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -12,4 +12,8 @@ describe "fizzbuzz" do
   it 'should return "fizzbuzz" if passed 15' do
     expect(fizzbuzz(15)).to eq "fizzbuzz"
   end
+
+  it 'should return n if passed 4' do
+    expect(fizzbuzz(4)).to eq 4
+  end
 end
